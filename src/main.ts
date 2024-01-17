10aa0e19d009bd5336d993d780c7b175e7cda11a
jakubmarchwiany <jakubmarchwiany@icloud.com>
2024-01-17 22:13:15 +0100

    feat: add secure connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from "@nestjs/core";
+import * as fs from "fs";
 
 import { AppModule } from "./app.module";
 
+const httpsOptions = {
+	cert: fs.readFileSync("secrets/ca.crt"),
+	key: fs.readFileSync("secrets/ca.key")
+};
+
 async function bootstrap(): Promise<void> {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { httpsOptions });
 
 	await app.listen(8080);
 }
