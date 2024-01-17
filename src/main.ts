0711526c00579b02c6de29add74e89102326414a
jakubmarchwiany <jakubmarchwiany@icloud.com>
2024-01-17 22:56:02 +0100

    revert: back to http connection (some error on front app)

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import * as fs from "fs";
 
 import { AppModule } from "./app.module";
 
-const httpsOptions = {
-	cert: fs.readFileSync("secrets/ca.crt"),
-	key: fs.readFileSync("secrets/ca.key")
-};
+// const httpsOptions = {
+// 	cert: fs.readFileSync("secrets/ca.crt"),
+// 	key: fs.readFileSync("secrets/ca.key")
+// };
 
 async function bootstrap(): Promise<void> {
-	const app = await NestFactory.create(AppModule, { httpsOptions });
+	const app = await NestFactory.create(AppModule);
+	// const app = await NestFactory.create(AppModule, { httpsOptions });
 
 	await app.listen(8080);
 }
