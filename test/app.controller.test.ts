2333e29ca45acb5005581ff071fd9c957f33c755
jakubmarchwiany <jakubmarchwiany@icloud.com>
2024-01-17 23:30:10 +0100

    feat: add tests (getWeather)

--- test/app.controller.test.ts
+++ test/app.controller.test.ts
@@ -2,8 +2,6 @@
 import { describe, expect, it } from "bun:test";
 import request, { Response } from "supertest";
 
-import { expectedBody } from "./utils";
-
 const API_URL = process.env.API_URL;
 
 console.log(API_URL);
@@ -16,6 +14,33 @@ describe("AppController", () => {
 			res = await request(API_URL!).get("/weather?cityName=warsaw");
 
 			expect(res.statusCode).toBe(200);
+
+			expect(res.body).toEqual({
+				cityName: expect.any(String),
+				condition: expect.any(String),
+				date: expect.any(String),
+				humidity: expect.any(Number),
+				temperature: expect.any(Number),
+				windSpeed: expect.any(Number)
+			});
 		});
+	});
+
+	it("should_return_400_for_invalid_data", async () => {
+		res = await request(API_URL!).get("/weather?cityName=warsawasddasdasd");
+
+		expect(res.statusCode).toBe(404);
+	});
+
+	it("should_return_400_for_missing_city_name", async () => {
+		res = await request(API_URL!).get("/weather");
+
+		expect(res.statusCode).toBe(400);
+	});
+
+	it("should_return_404_for_wrong_endpoint", async () => {
+		res = await request(API_URL!).get("/leathar");
+
+		expect(res.statusCode).toBe(404);
 	});
 });
